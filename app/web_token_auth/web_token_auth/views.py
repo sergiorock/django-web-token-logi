dbefa476ae7b9f80cb1423e38e0261649ca05fde
sergiorock <lopez.sergio.n@gmail.com>
2025-02-10 01:21:17 -0300

    protected profile view

--- app/web_token_auth/web_token_auth/views.py
+++ app/web_token_auth/web_token_auth/views.py
@@ -4,11 +4,23 @@ from .serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 
 @api_view(['POST'])
 def login(request):
-  return Response({})
+  user = get_object_or_404(User, username=request.data['username'])
+
+  if not user.check_password(request.data['password']):
+    return Response({'error': 'incorrect password'}, status=status.HTTP_401_UNAUTHORIZED)
+  
+  token, created = Token.objects.get_or_create(user=user)
+  serializer = UserSerializer(instance=user)
+
+  return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
@@ -23,5 +35,10 @@ def register(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
 def profile(request):
-  return Response({})
+  print(request.user)
+  serializer = UserSerializer(instance=request.user)
+
+  return Response(serializer.data, status=status.HTTP_200_OK)
